d2885e0e0dc13f296ae14af55627fe81891207ed
J0e101 <josephonuonga5@gmail.com>
2025-02-03 13:10:58 +0300

    Python

---
+++ introduction.py
@@ -0,0 +1,7 @@
+#Print is a method used to display a value or output
+print("J0e101")
+print("emobilis")
+print(39)
+print(51*2)
+print("10"+"2")
+
---
+++ variables.py
@@ -0,0 +1,13 @@
+#Variable is a container used to store value(s)
+a = 10
+b = 15
+Firstname = "Joseph"
+Language = "Python3"
+Lastname = "Billy"
+print(Firstname,Lastname)
+print(Language,"is a web programming language")
+print(a+b)
+
+#The sum of 10 and 15 is 25
+
+print("The sum of",a,"and",b,"is",a+b)
